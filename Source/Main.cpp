067f4c6f72bdb95b67cab82f1de9e7bbef22074f
SavageMocha <maxhayespersonal@gmail.com>
2022-05-14 20:56:35 -0700

    round 2

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -6,6 +6,7 @@
   ==============================================================================
 */
 
+#include <cassert>
 #include <JuceHeader.h>
 #include <type_traits>
 #include <typeinfo>
@@ -14,6 +15,9 @@
 //==============================================================================
 
 // Max sandbox type area (todo: setup unit test environment)
+template <typename T>
+class ParamType;
+
 
 // (concept)
 class Parameter
@@ -22,10 +26,13 @@ public:
     virtual ~Parameter() = default;
 
     // virtual interface
-    virtual const juce::Identifier& GetName() const = 0;
+    virtual juce::var GetVar() const = 0;
     virtual const std::type_info& GetType() const = 0;
-    virtual operator juce::String() const = 0; // for debug & UI purposes
-    
+    virtual const juce::Identifier& GetName() const = 0;
+
+    // template interface for runtime type un-erasure
+    template <typename T>
+    const T& Get() const;
 };
 
 
@@ -33,74 +40,39 @@ public:
 template <typename T>
 class ParamType : public Parameter
 {
+    // (data_ -> var)
+    // make sure juce::Var(T arg) will compile or we have a user-defined 'T::operator juce::var() const' availible
+    static_assert(std::is_constructible<T, juce::var>::value
+                || std::is_convertible<T, juce::var>::value);
+
+    // (var->data_)
+    // if juce::var(T arg) complies, we can find the correct juce::var::getAs
+    static_assert(std::is_constructible<T, juce::var>::value
+                || std::is_constructible<juce::var, T>::value); // user-provided constructor
+
 public:
     // ctor
-    ParamType(const juce::Identifier& Name, const T& InitialValue = {})
-    : data_(InitialValue)
-    , name_(Name)
+    ParamType(const juce::Identifier& Name)
+    : name_(Name)
     {}
-
-    ParamType(const juce::Identifier& Name, T&& InitialValue)
+    
+    ParamType(const juce::Identifier& Name, const T& InitialValue)
     : data_(InitialValue)
     , name_(Name)
     {}
 
     // dtor
     virtual ~ParamType() override = default;
-
-    // implicit cast to underlying type
-    operator T&() { return data_; }
-    operator T() { return data_; }
     
     // template interface
     T& operator*() { return data_; }
     const T& operator*() const { return data_; }
 
     // base interface
-    virtual const juce::Identifier& GetName() const override { return name_; }
+    virtual juce::var GetVar() const override { return juce::var(data_); }
     virtual const std::type_info& GetType() const override { return typeid(data_); }
-
-    // implicit cast to juce::String for logging / UI
-    virtual operator juce::String() const override
-    {
-        if constexpr(std::is_same<T, bool>::value)
-        {
-            return juce::String(data_? "true" : "false");
-        }
-        else if constexpr(std::is_same<T, std::vector<float>>::value) // todo: generalize string-ifying containers & arrays(?)
-        {
-            constexpr int previewLen = 3;
-            const int p = juce::jmin<int>(previewLen, static_cast<int>(data_.size()));
-
-            juce::String res;
-
-            res << "["<< data_.size() << "]";
-
-            if(p > 0)
-            {
-                res << "{ " << data_[0];
-                for(int i = 1; i < p; ++i)
-                {
-                     res << ", " << data_[i];
-                }
-
-                if(p < static_cast<int>(data_.size()))
-                {
-                    res << ", ...";
-                }
-
-                res << " }";
-            }
-
-            return res;
-        }
-        else
-        {
-            static_assert( std::is_convertible_v<T, juce::String> || std::is_constructible_v<juce::String, T>
-                , "underlying type must be convertable to juce::String, either add a constexpr case to this switch statement, or support implicit conversion" );
-            return juce::String(data_);
-        }
-    }
+    virtual const juce::Identifier& GetName() const override { return name_; }
+    
     
 private:
     T data_;
@@ -109,18 +81,35 @@ private:
 };
 
 
+// todo: make a safe std::optional version 
+template <typename T>
+const T& Parameter::Get() const
+{
+    auto* derivedPtr = dynamic_cast<const ParamType<T>*>(this);
+    jassert(derivedPtr); // type T does not match underlying PraamType :(
+
+    if(derivedPtr)
+    {
+        return **derivedPtr;
+    }
+
+    static T Failure;
+    return Failure;
+}
+
+
+
 class ParameterList : public std::vector<std::unique_ptr<Parameter>>
 {
 public:
-    // ...from other object
+    // ...from r-value other
     template <typename T>
-    ParameterList& add(const juce::Identifier Name, const T& InitialValue = {})
+    ParameterList& add(const juce::Identifier Name)
     {
-        push_back(std::make_unique<ParamType<T>>(Name, InitialValue));
+        push_back(std::make_unique<ParamType<T>>(Name));
         return *this;
     }
-
-    // ...from r-value other
+    
     template <typename T>
     ParameterList& add(const juce::Identifier Name, T&& InitialValue)
     {
@@ -128,14 +117,26 @@ public:
         return *this;
     }
 
-    // ...from explicit ctor args
-    template <typename T, typename... Args>
-    ParameterList& add(const juce::Identifier Name, Args... args)
+    juce::String DebugString(int AraryPrintDepth = 0) const
     {
-        push_back(std::make_unique<ParamType<T>>(args...));
-        return *this;
-    }
+        juce::String dbg;
+        
+        for(const auto& param : *this)
+        {
+            auto name = param->GetName();
+            juce::String type = param->GetType().name();
+            juce::String value = param->GetVar().toString();
 
+            if(param->GetVar().isArray() || AraryPrintDepth > 0)
+            {
+                // todo
+            }
+
+            dbg << '(' << type << ") " << name << " = " << value << '\n';
+        }
+
+        return dbg;
+    }
     
 private:
     
@@ -143,35 +144,35 @@ private:
 
 struct Position3D
 {
-    float x, y, z;
-    operator juce::String() const
-    {
-        juce::String res;
-        res << "[ X: "  << x << ", Y: " << y << ", Z: " << z << " ]";
+    double x, y, z;
 
-        return res;
+    operator juce::var() const
+    {
+        return juce::Array<juce::var>(x, y, z);
     }
 };
 
 struct Position2D
 {
-    float x, y;
-    operator juce::String() const
-    {
-        juce::String res;
-        res << "[ X: "  << x << ", Y: " << y << " ]";
+    double x = 0.0;
+    double y = 0.0;
 
-        return res;
+    operator juce::var() const
+    {
+        return juce::Array<juce::var>(x, y);
     }
 };
 
 
 int main (int, char*)
 {
+    std::cout << std::boolalpha;
+    
     // example data init
     std::vector<float> myBuffer;
     for(int i = 0; i < 25; ++i)
     {
+        juce::String MyFloatString(5.f);
         myBuffer.emplace_back(static_cast<float>(i) + 0.1f);
     }
 
@@ -179,36 +180,29 @@ int main (int, char*)
     ParameterList paramList;
     
     paramList
-        .add<float>("HPF frequency")
-        .add<float>("Num Taps", 4)
+        // .add<std::vector<float>>("buffer", { 1.0, 2.0, 3.0 })
+        // .add<std::vector<float>>("buffer", { 1.0 })
+        // .add<std::vector<float>>("buffer")
+        // .add<std::vector<float>>("buffer", { 1.0, 2.0 })
+        .add<Position3D>("Source Position3D", { 0.0, 25.0, 10.0 })
+        .add<Position2D>("Source Position2D", { 0.0, 25.0 })
         .add<bool>("Enabled?", false)
-        .add<Position3D>("Source Position3D", { 0.f, 25.f, 10.f })
-        .add<Position2D>("Source Position2D", { 0.f, 25.f })
-        .add<std::vector<float>>("buffer", myBuffer)
-        .add<std::vector<float>>("buffer", { 1.1f, 2.1f, 3.1f, 4.1f })
-        .add<std::vector<float>>("buffer", { 1.f, 2.f, 3.f })
-        .add<std::vector<float>>("buffer", { 1.f, 2.f })
-        .add<std::vector<float>>("buffer", { 1.f });
+        .add<int>("Num Taps", 4)
+        .add<float>("HPF frequency", 500.f);
 
 
-    // parameter list debug view
-    for(const auto& param : paramList)
-    {
-        auto name = param->GetName();
-        juce::String type = param->GetType().name();
-        juce::String value = *param;
-
-        DBG(name + " = " + value + " -- type: (" + type + ")");
-    }
+    DBG(paramList.DebugString());
 
     
     // parameter manipulation
-    ParamType<float> float1("My Float", 1.f);
-    ParamType<float> float2("My Float", 2.f);
+    ParamType<float> f("Freq.", 190.f);
+    Parameter* p = &f;
 
-    ParamType<float> float3("My Float");
-    *float3 = *float1 + *float2;
-    jassert(*float3 == 3.f);
+    float x = p->Get<float>(); // should compile
+    double y = p->Get<float>(); // should compile & cast w/ warning
+    int z = p->Get<int>(); // should hit runtime assert
+    
+    
 
     return 0;
 }
