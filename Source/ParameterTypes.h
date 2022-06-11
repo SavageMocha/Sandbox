7567b46f3c609a7cd714f2b3253f89e142bf749e
SavageMocha <maxhayespersonal@gmail.com>
2022-06-10 21:15:06 -0700

    added getters for refrences. started working on juce serialization

--- Source/ParameterTypes.h
+++ Source/ParameterTypes.h
@@ -18,21 +18,25 @@ namespace Haze
 // forwards
   template <typename T>
   class ParamType;
-  class Parameter;
+  class UiParameter;
 
 
   // (concept)
-  class Parameter
+  class UiParameter
   {
   public:
-    virtual ~Parameter() = default;
+    virtual ~UiParameter() = default;
 
     // common interface
     [[nodiscard]] virtual const type_info& Type() const = 0;
 
-    // assignmentt
+    // to/from juce::var (serialization)
+    [[nodiscard]] virtual juce::var GetAsVar() const = 0;
+    virtual void SetAsVar(const juce::var& inVar) = 0;
+
+    // assignment
     template <typename T>
-    Parameter& operator=(const T& inValue)
+    UiParameter& operator=(const T& inValue)
     {
       if(auto* downPtr = DowncastChecked<T>())
       {
@@ -61,6 +65,13 @@ namespace Haze
       return **DowncastChecked<T>();
     }
 
+    template <typename T>
+    T& GetRef()
+    {
+      return **DowncastChecked<T>();
+    }
+  
+
   private:
     
     // downcast helpers (runtime assert on failure)
@@ -86,13 +97,35 @@ namespace Haze
 
   
   template <typename T>
-  class ParamType : public Parameter
+  class ParamType : public UiParameter
   {
   public:
     ParamType() = delete;
-    ParamType(T&& value) : Parameter(), data_(value) {}
+    ParamType(T&& value) : UiParameter(), data_(value) {}
     
     [[nodiscard]] const type_info& Type() const override final { return typeid(T); }
+
+    [[nodiscard]] virtual juce::var GetAsVar() const
+    {
+      static_assert(
+        std::is_constructible<juce::var, T>::value || std::is_convertible<T, juce::var>::value);
+      
+      if constexpr (std::is_constructible<juce::var, T>::value)
+      {
+        return juce::var(data_);
+      }
+      else if constexpr (std::is_convertible<T, juce::var>::value)
+      {
+        return static_cast<juce::var>(data_);
+      }
+
+      return {};
+    }
+    
+    virtual void SetAsVar(const juce::var& inVar)
+    {
+      data_ = inVar;
+    } 
 
 
     ParamType& operator=(const T& inValue)
@@ -106,45 +139,87 @@ namespace Haze
       return data_;
     }
 
+    T& operator*()
+    {
+      return data_;
+    }
+
   private:
     T data_;
     
   }; // ParamType<T>
-
   
 
-  using ParamListEntryType = std::unordered_map<std::string, std::unique_ptr<Parameter>>;
-
-  // todo: instead of elements being the raw unique_ptr,
-  // wrap that in a struct that hides the ptr-deref syntax, and has ui meta-data
-  // a similar struct can be handed to a ParameterList::add() overload
+  using ParamListEntryType = std::unordered_map<std::string, std::unique_ptr<UiParameter>>;
+  
   class ParameterList : public ParamListEntryType
   {
   public:
-
-    // builder  
+    // builder methods
+    // add w/ juce::Identifier
     template <typename T>
     ParameterList& add(juce::Identifier Name, T&& DefaultValue = {})
     {
-      // todo: disallow duplicate entries (name collision)
-      this->ParamListEntryType::operator[](Name.toString().toStdString())
-        = std::make_unique<ParamType<T>>(std::forward<T>(DefaultValue));
-      return *this;
+      std::string str = Name.toString().toStdString();
+      return add(str, std::forward<T>(DefaultValue));
     }
 
+    // add w/ std::string
     template <typename T>
     ParameterList& add(std::string& Name, T&& DefaultValue = {})
     {
+      // name collision (previous entry will be stomped!)
+      jassert(this->find(Name) == this->end());
+      
       this->ParamListEntryType::operator[](Name)
         = std::make_unique<ParamType<T>>(std::forward<T>(DefaultValue));
       return *this;
     }
 
-    std::unique_ptr<Parameter>& operator[](const juce::Identifier Name)
+    // add + get ref w/ juce::identifier
+    template <typename T>
+    T& addAndGetRef(juce::Identifier Name, T&& DefaultValue = {})
+    {
+      return addAndGetRef(Name.toString().toStdString(), std::forward<T>(DefaultValue));
+    }
+
+    // add + get ref w/ std::string
+    template <typename T>
+    T& addAndGetRef(std::string& Name, T&& DefaultValue = {})
+    {
+      std::unique_ptr<UiParameter> NewEntry = std::make_unique<ParamType<T>>(std::forward<T>(DefaultValue));
+      
+      T& ref = NewEntry->GetRef<T>;
+      this->ParamListEntryType::operator[](Name) = std::move(NewEntry);
+
+      return ref;
+    }
+
+    // index operator oveload for juce::Identifier
+    std::unique_ptr<UiParameter>& operator[](const juce::Identifier Name)
     {
       return this->ParamListEntryType::operator[](Name.toString().toStdString());
     }
+
     
+    juce::ValueTree BootstrapValueTree() const
+    {
+      static juce::Identifier ParamList("Parameter_List");
+      juce::ValueTree listTree(ParamList);
+      
+      for(const auto& entry : *this)
+      {
+        // todo: need access to a "to juce::var" function
+        listTree.setProperty({entry.first}, juce::var(entry.second->GetAsVar()), nullptr);
+      }
+
+      return listTree;
+    }
+
+    void SyncToTree(juce::ValueTree inTree)
+    {
+      // todo
+    }
     
   }; //ParameterList
 } // namespace Haze
--- Source/UnitTest_ParameterTypes.cpp
+++ Source/UnitTest_ParameterTypes.cpp
@@ -20,27 +20,27 @@ namespace Haze
     // ...easily declare a list of parameters (w/ unique names) via builder pattern
     beginTest("Parameter list declaration");
 
-    const juce::Identifier Freq("freq.");
+    const juce::Identifier Freq("freq");
 
     ParameterList param_list;
     param_list
       .add(Freq, 500.f)
-      .add("Num Taps", 4)
+      .add("Num_Taps", 4)
     ;
 
     // ...Assign to/from the underlying data using operator[] and operator=
     //      and compare the entry w/ a value of the same type
-    int x = param_list["Num Taps"]->Get<int>();
+    beginTest("Parameter Get, Set, and Comparison");
+    int x = param_list["Num_Taps"]->Get<int>();
     expect(x == 4);
 
-    const bool bIsFour = param_list["Num Taps"]->IsEqualTo(4);
+    const bool bIsFour = param_list["Num_Taps"]->IsEqualTo(4);
     expect(bIsFour);
 
-    *param_list["Num Taps"] = 10;
-    expect(param_list["Num Taps"]->Get<int>() == 10);
-    expect(param_list["Num Taps"]->IsEqualTo(10));
-
-
+    *param_list["Num_Taps"] = 10;
+    expect(param_list["Num_Taps"]->Get<int>() == 10);
+    expect(param_list["Num_Taps"]->IsEqualTo(10));
+    
     float y = param_list[Freq]->Get<float>();
     expect(y == 500.f);
 
@@ -50,26 +50,34 @@ namespace Haze
     *param_list[Freq] = 15.f;
     expect(param_list[Freq]->Get<float>() == 15.f);
     expect(param_list[Freq]->IsEqualTo(15.f));
-    
 
-    
+    beginTest("Parameter GetRef(), (access updated value without indexing)");
+    int& xRef = param_list["Num_Taps"]->GetRef<int>();
+    int currX = xRef;
 
-    // ...cache a read-only reference to the value
-    // what will be updated in a theard safe way when value tree updates
-    beginTest("ParameterList -> (default) ValueTree");
-    
+    *param_list["Num_Taps"] = 111;
+    expect(currX != xRef);
+    expect(xRef == 111);
+
+    xRef = 222;
+    expect(param_list["Num_Taps"]->IsEqualTo(222) == true);
+
 
     // ...bootstrap a juce::ValueTree from that list
-    beginTest("ParameterList -> (default) ValueTree");
+    beginTest("Parameter GetRef(), (access updated value without indexing)");
+    auto xmlString = param_list.BootstrapValueTree().toXmlString();
+    DBG(xmlString);
+
+    expect(xmlString.isEmpty() == false);
 
 
     // ...synchronize my internal parameters to a juce::ValueTree
+
+    
     // ...so that when the value tree chanegs, my parameters will update internally
-    beginTest("ParameterList::Synchonize(juce::ValueTree);");
 
 
     // ...retrieve the up-to-date underlying data in a thread safe way (for audio processing)
-    beginTest("Thread-safe get()");
     
   }
   
@@ -80,7 +88,7 @@ namespace Haze
 
 // random thought:
 /*
- *  it would be badass to have separate execution edges for different threadsh
+ *  it would be badass to have separate execution edges for different threads
  *  (i.e. UI thread vs Audio thread)
  *  Data could be exchanged, but execution graphs would be separate
 */
